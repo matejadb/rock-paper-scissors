c7b029f2cee57b56c11c15d0ac9f04b0a3d3e973
matejadb <92729211+matejadb@users.noreply.github.com>
2024-10-14 13:12:10 +0200

    Add getHumanChoice() function
    Added a function that gets a human players input. The input is limited to numbers from 1 to 3.

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ function getComputerChoice() {
 
   return Math.floor(Math.random() * (3 - 2 + 1)) + 1;
 }
+
+function getHumanChoice() {
+  /*
+    Human Choice also has 3 values. Human inputs the choice into the prompt window.
+    The choices have to be numbers in between 1 and 3. 
+    IF the numbers are less than 1 OR higher than 3 THEN the user has to input the choice again 
+  */
+
+  function getUserInput() {
+    var userInput = parseInt(
+      prompt("Enter your choice: 1 - Rock, 2 - Paper, 3 - Scissors")
+    );
+
+    return userInput;
+  }
+
+  var input = getUserInput();
+
+  while (input < 1 || input > 3) {
+    input = getUserInput();
+  }
+
+  return input;
+}
