5e76e04e9c0a629657bb13f06ee5e0eb59eb0e44
matejadb <92729211+matejadb@users.noreply.github.com>
2024-10-14 13:02:10 +0200

    Add getComputerChoice() function
    Added a function that generates a choice for the computer.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Test");
+function getComputerChoice() {
+  /*
+  Computer Choice can have 3 values (Rock, Paper or Scissors). They are represented by randomly generated numbers, like:
+    1 - Rock
+    2 - Paper
+    3 - Scissors
+  */
+
+  return Math.floor(Math.random() * (3 - 2 + 1)) + 1;
+}
