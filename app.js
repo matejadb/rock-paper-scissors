90e021d4608c71db8a6c1b5f05fa9309cd930992
matejadb <92729211+matejadb@users.noreply.github.com>
2024-10-17 14:26:25 +0200

    Add playRound()
    Implemented basic game logic using if, else if.

--- app.js
+++ app.js
@@ -27,9 +27,36 @@ function getHumanChoice() {
   return userInput;
 }
 
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! Paper beats rock!");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice == "scissors") {
+    console.log("You lose! Scissors beats paper!");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats scissors!");
+    humanScore++;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats rock!");
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! Scissors beats paper!");
+    humanScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats scissors!");
+    computerScore++;
+  } else {
+    console.log("It's a draw!");
+  }
+}
+
 var humanScore = 0;
 var computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = getHumanChoice().toLowerCase();
-}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
